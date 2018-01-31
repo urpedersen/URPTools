6d0a50fb7bbfbf1735cd3eed86a2df56766d1d23
Ulf R. Pedersen <ulf@urp.dk>
2018-01-31 17:01:44 +0100

    Update randomGaussian.cc
    removed som old unused code

--- make_lattice/src/randomGaussian.cc
+++ make_lattice/src/randomGaussian.cc
@@ -13,11 +13,5 @@ double randomGaussian() {
     v = 2.0*(double)rand()/(double)RAND_MAX-1.0;
     s = u*u + v*v;
   } while( s >= 1.0 || s == 0.0 );
-  return u*sqrt(-2.0*log(s)/s);  
-  // Note: an other random number is avalible, v*sqrt(-2.0*log(s)/s)
-  
-  // Below is an implimentation using a sum of 12 random numbers
-  //double out = 0.0;
-  //for(unsigned i=0;i<12;i++) out+=((double)rand()/(double)RAND_MAX-0.5); // Normal distribution with sigma=1
-  //return out;
+  return u*sqrt(-2.0*log(s)/s);
 }
